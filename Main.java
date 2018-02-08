281b8e3381033b25455ab586c33461fae630e96b
toame <36276854+toame@users.noreply.github.com>
2018-02-09 05:05:52 +0900

    Main

--- Main.java
+++ Main.java
@@ -3,10 +3,11 @@ public class Main{
         int a = sc.nextInt();
         for (int i = 2; i < a; i++) {
             if (a % i == 0) {
-                System.out.println(a + "は素数ではありません");
+                System.out.println(a + "は素数ではありません.");
+                System.out.println(a + "は合成数ってことだ.");
                 return;
             }
         }
-        System.out.println(a + "は素数です");
+        System.out.println(a + "は素数です.");
     }
 }
